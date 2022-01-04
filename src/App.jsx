caaa5869ef88fd04af75124b9aecb40e5344c202
Anita Schiller <anita.s1@gmx.de>
2022-01-04 12:53:51 +0100

    Setup storybook with vite

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import Card from './components/Card';
+
+function App() {
+  return (
+    <div className="App">
+      <Card title="Test" color="#49B8B0" />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/Card.jsx
@@ -0,0 +1,46 @@
+import PropTypes from 'prop-types';
+import { darken } from 'polished';
+import styled from 'styled-components';
+
+export default function Card({
+  title,
+  text,
+  author,
+  color = 'papayawhip',
+  isActive = true,
+}) {
+  return (
+    <Wrapper backgroundColor={color} isActive={isActive}>
+      <Title textColor={color}>{title}</Title>
+      <p>{text}</p>
+      <small>{author ? author : 'Anonymous'}</small>
+    </Wrapper>
+  );
+}
+
+Card.propTypes = {
+  title: PropTypes.string,
+  text: PropTypes.string,
+  author: PropTypes.string,
+  color: PropTypes.string,
+  isActive: PropTypes.bool,
+};
+
+const Wrapper = styled.section`
+  padding: 1rem;
+  background: ${(props) =>
+    props.backgroundColor ? props.backgroundColor : 'none'};
+  border: ${(props) =>
+    props.backgroundColor
+      ? darken(0.1, props.backgroundColor) + ' 5px solid'
+      : 'none'};
+  border-radius: 6px;
+  opacity: ${(props) => (props.isActive ? 1 : 0.5)};
+`;
+
+const Title = styled.h2`
+  color: ${(props) =>
+    props.textColor ? darken(0.3, props.textColor) : 'black'};
+  font-weight: normal;
+  margin: 0;
+`;
